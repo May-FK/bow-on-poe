9108e1e5a3da29233527ce2ebff9806444e97f9e
SunnyValentine <66551747+SunnyValentine@users.noreply.github.com>
2020-12-09 01:40:16 -0800

    Updates again

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void RestartLevel(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
